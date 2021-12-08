ff411bbe9386e26e1657c5c1f8b859a176d7c3fd
¨Venturin¨ <diegoventurinn@gmail.com>
2021-12-08 15:46:50 -0300

    Imprementado o requisito 01.

--- src/average.js
+++ src/average.js
@@ -12,7 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
 
+  if (array.length === 0) {
+    return;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return;
+    }
+    soma += array[i];
+  }
+
+  return Math.round(soma / array.length);
+};
 
 module.exports = average;
