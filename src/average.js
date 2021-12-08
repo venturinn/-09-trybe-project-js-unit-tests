0d822b09ddbfa5e1ef849bc472fe2eadefd2f950
¨Venturin¨ <diegoventurinn@gmail.com>
2021-12-08 16:11:31 -0300

    Implementado o requisito 02.

--- src/average.js
+++ src/average.js
@@ -16,12 +16,12 @@ const average = (array) => {
   let soma = 0;
 
   if (array.length === 0) {
-    return;
+    return undefined;
   }
 
   for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
-      return;
+      return undefined;
     }
     soma += array[i];
   }
--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -1,7 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const numbers = require('../src/numbers');
+const { TestScheduler } = require("jest");
+const numbers = require("../src/numbers");
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
@@ -14,12 +15,20 @@ const numbers = require('../src/numbers');
 
 */
 
-describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-    fail('Teste vazio!');
+describe("2 - Implemente os casos de teste para a função `numbers`", () => {
+  it("Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário", () => {
+    //fail('Teste vazio!');
+
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+       expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+  
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+      expect(numbers([1, 2, "3", 4, 5])).toEqual(false);
+
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+      expect(numbers([1, "a", 3])).toEqual(false);
+
     // Escreva um teste em que a função recebe [' '] e retorna false
+      expect(numbers([" "])).toEqual(false);
   });
 });
